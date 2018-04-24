ecaee462bd298bcbe6448ca74cfe267868a97444
Björn Büttner <idrinth@users.noreply.github.com>
2018-04-24 11:27:44 +0200

    removing useless json_decode

--- src/Controller/Request.php
+++ src/Controller/Request.php
@@ -9,12 +9,20 @@ class Request extends Controller
     public function add()
     {
         return $this->response
-            ->setJsonContent(json_decode('{"name":"Björn","id":1,"decider":false}'));
+            ->setJsonContent([
+                "name" => "Björn",
+                "id" => 1,
+                "decider" => false
+            ]);
     }
     public function index()
     {
         return $this->response
-            ->setJsonContent(json_decode('[{"id":1,"name":"example/example1","vote":1},{"id":2,"name":"example/example1","vote":-1},{"id":3,"name":"example/example2","vote":-2}]'));
+            ->setJsonContent([
+                ["id" => 1,"name" => "example/example1","vote" => 1],
+                ["id" => 2,"name" => "example/example1","vote" => -1],
+                ["id" => 3,"name" => "example/example2","vote" => -2]
+            ]);
     }
     public function vote(int $id)
     {
@@ -24,6 +32,17 @@ class Request extends Controller
     public function detail(int $id)
     {
         return $this->response
-            ->setJsonContent(json_decode('{"id":'.$id.',"description":"Hi there, this is a description","created":"2019-11-19 23:59:59","watchers":2}'));
+            ->setJsonContent([
+                "id" => $id,
+                "description" => "Hi there, this is a description",
+                "created" => "2019-11-19 23:59:59",
+                "watchers" => 2,
+                "score" => 1,
+                "repositories" => [
+                    "example/lib2" => "MIT",
+                    "example/lib12" => null,
+                    "exa/prop" => ["MIT", "LGPL"]
+                ]
+            ]);
     }
 }
